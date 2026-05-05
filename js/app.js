1fbaad25a521515d664b318f3960074e731d8812
belelalu <bele.lalu@gmail.com>
2026-05-05 15:23:22 +0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// MODAL CUCIAN
+function openCucian() {
+  document.getElementById("modalCucian").classList.remove("hidden");
+}
+
+function closeCucian() {
+  document.getElementById("modalCucian").classList.add("hidden");
+}
