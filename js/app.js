39b966766eb675406f992c89218831c63886bbba
belelalu <bele.lalu@gmail.com>
2026-05-05 15:45:36 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ function openCucian() {
 function closeCucian() {
   document.getElementById("modalCucian").classList.add("hidden");
 }
+
+function openCustomer() {
+  document.getElementById("modalCustomer").classList.remove("hidden");
+}
+
+function closeCustomer() {
+  document.getElementById("modalCustomer").classList.add("hidden");
+}
