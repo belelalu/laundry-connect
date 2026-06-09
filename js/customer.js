9e64c4c4a14972045460e974ae4c58e10ca78b9a
belelalu <bele.lalu@gmail.com>
2026-06-09 16:02:15 +0700

    Update customer.js

--- js/customer.js
+++ js/customer.js
@@ -32,39 +32,38 @@ window.loadCustomers = async function loadCustomers() {
     document.getElementById("tbodyCustomer");
 
   if (tbody) {
-
+    
     tbody.innerHTML = customers.map(c => `
-
-      <tr class="border-b hover:bg-gray-50">
-
-        <td class="p-4">
-          ${c.nama}
-        </td>
-
-        <td class="p-4">
-          ${c.no_wa || "-"}
-        </td>
-
-        <td class="p-4">
-          ${c.alamat || "-"}
-        </td>
-
-        <td class="p-4">
-
-          <button
-            class="text-blue-600 mr-3">
-            Detail
-          </button>
-
-          <button
-            class="text-red-600">
-            Hapus
-          </button>
-
-        </td>
-
-      </tr>
-
+    
+    <tr class="border-b hover:bg-gray-50">
+    
+    <td class="p-4 font-medium">
+    ${c.nama}
+    </td>
+    
+    <td class="p-4">
+    ${c.no_wa || "-"}
+    </td>
+    
+    <td class="p-4">
+    ${c.alamat || "-"}
+    </td>
+    
+    <td class="p-4">
+    ${c.created_at || "-"}
+    </td>
+    
+    <td class="p-4">
+    <button
+    onclick="detailCustomer('${c.id}')"
+    class="text-blue-600 mr-3">
+    Detail
+    </button>
+    
+    </td>
+    
+    </tr>
+    
     `).join("");
 
   }
