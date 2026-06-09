f5e9e8cfc4e1cda9da07f34d1e319dcb3aabeecc
belelalu <bele.lalu@gmail.com>
2026-06-09 12:09:56 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,12 +2,16 @@ const API_URL =
   "https://script.google.com/macros/s/AKfycbxZVQxpqUvh-T7yBjhwH1XATE50c67hOaU9StNZBvMvUfd4kK-MAVSbNa1CY65PS8Q/exec";
 
 async function gas(func, data = {}) {
-  const params = new URLSearchParams({
-    func,
-    ...data
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      func,
+      ...data
+    })
   });
-
-  const res = await fetch(API_URL + "?" + params.toString());
 
   return await res.json();
 }
