813d3020caf86e0adf6d8a02f88a68d43a1d4fc2
belelalu <bele.lalu@gmail.com>
2026-06-09 12:36:28 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ const API_URL =
   "https://script.google.com/macros/s/AKfycbxZVQxpqUvh-T7yBjhwH1XATE50c67hOaU9StNZBvMvUfd4kK-MAVSbNa1CY65PS8Q/exec";
 
 async function gas(f, d = {}) {
-  const res = await fetch(URL, {
+  const res = await fetch(API_URL, {   // ✅ FIX DI SINI
     method: "POST",
     headers: {
       "Content-Type": "text/plain;charset=utf-8"
