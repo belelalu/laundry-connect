14d5b8b9de9f08f64cf4f8e454ecfb45f43790ac
belelalu <bele.lalu@gmail.com>
2026-06-09 11:35:47 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,17 +1,13 @@
-window.API_URL =
+const API_URL =
   "https://script.google.com/macros/s/AKfycbxZVQxpqUvh-T7yBjhwH1XATE50c67hOaU9StNZBvMvUfd4kK-MAVSbNa1CY65PS8Q/exec";
 
-window.gas = async function (func, data = {}) {
-  const res = await fetch(window.API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      func,
-      ...data
-    })
+async function gas(func, data = {}) {
+  const params = new URLSearchParams({
+    func,
+    ...data
   });
 
+  const res = await fetch(`${API_URL}?${params.toString()}`);
+
   return await res.json();
-};
+}
