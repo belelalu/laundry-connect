a20e66162e7b2ce1828d55b01b7f6a29cf5480cb
belelalu <bele.lalu@gmail.com>
2026-06-11 10:51:59 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -171,14 +171,10 @@ if (mobile) {
     if (force) {
       sidebar.classList.remove("-translate-x-full");
       document.querySelector("main").style.width = "100%";
-      document.getElementById("mainContent").style.flexBasis = "100%";
-      document.getElementById("mainContent").style.maxWidth = "100%";
       if (overlay) overlay.classList.remove("hidden");
     } else {
       sidebar.classList.add("-translate-x-full");
       document.querySelector("main").style.width = "100%";
-      document.getElementById("mainContent").style.flexBasis = "100%";
-      document.getElementById("mainContent").style.maxWidth = "100%";
       if (overlay) overlay.classList.add("hidden");
     }
 
@@ -188,14 +184,10 @@ if (mobile) {
   if (isHidden) {
     sidebar.classList.remove("-translate-x-full");
     document.querySelector("main").style.width = "100%";
-    document.getElementById("mainContent").style.flexBasis = "100%";
-    document.getElementById("mainContent").style.maxWidth = "100%";
     if (overlay) overlay.classList.remove("hidden");
   } else {
     sidebar.classList.add("-translate-x-full");
     document.querySelector("main").style.width = "100%";
-    document.getElementById("mainContent").style.flexBasis = "100%";
-    document.getElementById("mainContent").style.maxWidth = "100%";
     if (overlay) overlay.classList.add("hidden");
   }
 
